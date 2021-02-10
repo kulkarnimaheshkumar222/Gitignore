91654cbdba2860c578fde84e1bb9300a1fb57f28
Mahesh Kulkarni <mahesh@Admins-iMac.local>
2021-02-10 22:27:43 +0530

    initial commit

---
+++ Gitignore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gitignore
+//
+//  Created by Mahesh Kulkarni on 10/02/21.
+//  Copyright © 2021 Mahesh Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
